f0d82656d785435aafb312c1194fe3c8d2c7b619
Marco Seravalli <seravalli@google.com>
2022-10-28 09:32:00 +0200

    Refactor to improve structure.
    Create small abstraction to be hide how the cache works.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use atty::Stream;
-use dirs;
 use std::collections::hash_map::DefaultHasher;
 use std::fs::File;
 use std::hash::Hasher;
@@ -10,73 +9,96 @@ use std::process::Command;
 use std::process::Output;
 use std::process::Stdio;
 
-fn prefix() -> String {
-    format!("{}/.caru/", dirs::home_dir().unwrap().to_str().unwrap())
+struct Entry {
+    stderr_file: File,
+    stdout_file: File,
 }
 
-fn read_from_cache(full_cmd_hash: u64) -> io::Result<()> {
-    let stderr_filename = format!("{}{:x}.stderr", prefix(), full_cmd_hash);
-    let stdout_filename = format!("{}{:x}.stdout", prefix(), full_cmd_hash);
-
-    let mut stderr_file = File::open(stderr_filename)?;
-    let mut stdout_file = File::open(stdout_filename)?;
-
-    const BUF_SIZE: usize = 2 << 12; // 4 KiB
-    let mut buffer = [0u8; BUF_SIZE];
-
-    loop {
-        match stderr_file.read(&mut buffer) {
-            Ok(0) => break,
-            Ok(read_bytes) => {
-                let read_buf = &buffer[..read_bytes];
-                io::stdout().write_all(read_buf)?;
-            }
-            Err(e) => print!("{}", e),
+impl Entry {
+    pub fn new(stderr_file: File, stdout_file: File) -> Self {
+        Self {
+            stderr_file,
+            stdout_file,
         }
     }
-    // io::stdout().flush()?;
 
-    loop {
-        match stdout_file.read(&mut buffer) {
-            Ok(0) => break,
-            Ok(read_bytes) => {
-                let read_buf = &buffer[..read_bytes];
-                io::stdout().write_all(read_buf)?;
+    pub fn write_to_stderr_stdout(&mut self) -> io::Result<()> {
+        const BUF_SIZE: usize = 2 << 13; // 8 KiB
+        let mut buffer = [0u8; BUF_SIZE];
+
+        loop {
+            match self.stderr_file.read(&mut buffer) {
+                Ok(0) => break,
+                Ok(read_bytes) => {
+                    let read_buf = &buffer[..read_bytes];
+                    io::stdout().write_all(read_buf)?;
+                }
+                Err(e) => print!("{}", e),
             }
-            Err(e) => print!("{}", e),
+        }
+        // io::stdout().flush()?;
+
+        loop {
+            match self.stdout_file.read(&mut buffer) {
+                Ok(0) => break,
+                Ok(read_bytes) => {
+                    let read_buf = &buffer[..read_bytes];
+                    io::stdout().write_all(read_buf)?;
+                }
+                Err(e) => print!("{}", e),
+            }
+        }
+        // io::stdout().flush()?;
+
+        Ok(())
+    }
+}
+
+struct Cache {
+    prefix: String,
+}
+
+impl Cache {
+    pub fn new() -> Self {
+        Self {
+            prefix: format!("{}/.caru/", dirs::home_dir().unwrap().to_str().unwrap()),
         }
     }
-    // io::stdout().flush()?;
 
-    Ok(())
-}
+    fn try_get_stderr_stout_files(&self, entry_hash: u64) -> io::Result<(File, File)> {
+        let stderr_filename = format!("{}{:x}.stderr", self.prefix, entry_hash);
+        let stdout_filename = format!("{}{:x}.stdout", self.prefix, entry_hash);
 
-// TODO improve get_cache_result api to return an optional
-fn get_cache_result(full_cmd_hash: u64) -> io::Result<()> {
-    let stderr_filename = format!("{}{:x}.stderr", prefix(), full_cmd_hash);
-    let stdout_filename = format!("{}{:x}.stdout", prefix(), full_cmd_hash);
+        let stderr_file = File::open(stderr_filename)?;
+        let stdout_file = File::open(stdout_filename)?;
 
-    let _stderr_file = File::open(stderr_filename)?;
-    let _stdout_file = File::open(stdout_filename)?;
+        Ok((stderr_file, stdout_file))
+    }
 
-    Ok(())
-}
+    pub fn get(&self, entry_hash: u64) -> io::Result<Option<Entry>> {
+        match self.try_get_stderr_stout_files(entry_hash) {
+            Ok((stderr_file, stdout_file)) => Ok(Some(Entry::new(stderr_file, stdout_file))),
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => Ok(None),
+                _ => Err(e),
+            },
+        }
+    }
 
-// TODO: parallelize writing to stdout/stderr and to files
-fn store_output(process_output: &Output, full_cmd_hash: u64) -> io::Result<()> {
-    // TODO: mkdir -p
+    // TODO: parallelize writing to stdout/stderr and to files
+    pub fn insert(&self, process_output: &Output, full_cmd_hash: u64) -> io::Result<()> {
+        let stderr_filename = format!("{}{:x}.stderr", self.prefix, full_cmd_hash);
+        let mut stderr_chache = File::create(stderr_filename)?;
+        stderr_chache.write_all(&process_output.stderr)?;
+        // stderr_chache.flush()?;
 
-    let stderr_filename = format!("{}{:x}.stderr", prefix(), full_cmd_hash);
-    let mut stderr_chache = File::create(stderr_filename)?;
-    stderr_chache.write_all(&process_output.stderr)?;
-    // stderr_chache.flush()?;
+        let stdout_filename = format!("{}{:x}.stdout", self.prefix, full_cmd_hash);
+        let mut stdout_chache = File::create(stdout_filename)?;
+        stdout_chache.write_all(&process_output.stdout)?;
+        // stdout_chache.flush()?;
 
-    let stdout_filename = format!("{}{:x}.stdout", prefix(), full_cmd_hash);
-    let mut stdout_chache = File::create(stdout_filename)?;
-    stdout_chache.write_all(&process_output.stdout)?;
-    // stdout_chache.flush()?;
-
-    Ok(())
+        Ok(())
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -86,75 +108,57 @@ fn main() -> io::Result<()> {
         Some(io::stdin().lock())
     };
 
+    let cache = Cache::new();
+
     let cmd = std::env::args()
         .skip(1)
         .fold(String::new(), |acc, s| format!("{} {}", acc, s));
 
-    let mut args_hasher = DefaultHasher::new();
-    args_hasher.write(cmd.as_bytes());
-    let args_hash = args_hasher.finish();
+    let mut hasher = DefaultHasher::new();
+    hasher.write(cmd.as_bytes());
 
     let mut process = Command::new("sh")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .arg("-c")
         .arg(cmd)
-        .spawn()
-        .unwrap();
+        .spawn()?;
 
-    const BUF_SIZE: usize = 2 << 12; // 4 KiB
+    const BUF_SIZE: usize = 2 << 13; // 8 KiB
     let mut buffer = [0u8; BUF_SIZE];
 
-    let mut stdin_hasher = DefaultHasher::new();
-
-    let stdin_hash = match handle {
-        Some(mut h) => {
-            loop {
-                match h.read(&mut buffer) {
-                    Ok(0) => break,
-                    Ok(read_bytes) => {
-                        let read_buf = &buffer[..read_bytes];
-                        process.stdin.as_mut().unwrap().write(read_buf).unwrap();
-                        stdin_hasher.write(read_buf);
-                    }
-                    Err(e) => print!("{}", e),
-                };
-            }
-            stdin_hasher.finish()
+    if let Some(mut h) = handle {
+        loop {
+            match h.read(&mut buffer) {
+                Ok(0) => break,
+                Ok(read_bytes) => {
+                    let read_buf = &buffer[..read_bytes];
+                    process.stdin.as_mut().unwrap().write_all(read_buf)?;
+                    hasher.write(read_buf);
+                }
+                Err(e) => print!("{}", e),
+            };
         }
-        None => 0,
-    };
+    }
 
     // TODO: investigate dropping stdin: according to
     // https://stackoverflow.com/questions/49218599/write-to-child-process-stdin-in-rust we might
     // nedd to drop stdin
 
-    let mut full_cmd_hasher = DefaultHasher::new();
-    full_cmd_hasher.write_u64(stdin_hash);
-    full_cmd_hasher.write_u64(args_hash);
+    let full_cmd_hash = hasher.finish();
 
-    let full_cmd_hash = full_cmd_hasher.finish();
-
-    match get_cache_result(full_cmd_hash) {
-        Ok(()) => {
-            // println!("Command was already executed!");
-            process.kill().unwrap();
-            read_from_cache(full_cmd_hash).unwrap();
-        }
-        Err(e) => match e.kind() {
-            std::io::ErrorKind::NotFound => {
-                // println!("New execution")
-            }
-            e => println!("{}", e),
-        },
+    if let Some(mut entry) = cache.get(full_cmd_hash)? {
+        // println!("Command was already executed!");
+        process.kill()?;
+        entry.write_to_stderr_stdout()?;
     };
 
-    let process_output = process.wait_with_output().unwrap();
+    let process_output = process.wait_with_output()?;
     if process_output.status.success() {
-        store_output(&process_output, full_cmd_hash).unwrap();
+        cache.insert(&process_output, full_cmd_hash)?;
 
-        io::stdout().write_all(&process_output.stderr).unwrap();
-        io::stdout().write_all(&process_output.stdout).unwrap();
+        io::stdout().write_all(&process_output.stderr)?;
+        io::stdout().write_all(&process_output.stdout)?;
     }
     Ok(())
 }
